a56bc97cb2a0e97fba457eda94c9e544b2b23b85
Winifred Frierson <winifred.dykes@hotmail.com>
2019-12-19 09:42:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,7 +34,6 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  shipping_manifest{"muskets" => "2", "gun powder" => "4"}
   shipping_manifest["muskets"] = "2"
   shipping_manifest["gun powder"] = "4"
   
